97ed45be03a8f6c5e52a9e352d2af1e64b6b5e91
Nikita <99484973+Nikita-Isaichenko@users.noreply.github.com>
2024-11-30 02:56:56 +0700

    fix(): Исправлен баг со звуком

--- BAFLB/Data/ApplicationContext.cs
+++ BAFLB/Data/ApplicationContext.cs
@@ -4,18 +4,13 @@ namespace BAFLB.Data
 {
     public class ApplicationContext : DbContext
     {
-        public DbSet<User> Users { get; set; } = null;
+        public DbSet<User> Users { get; set; }
 
         public DbSet<Round> Rounds { get; set; }
 
-        public ApplicationContext() 
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
-            
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=baflb;Username=postgres;Password=Vladik@2003");
+            Database.EnsureCreated();
         }
     }
 }
